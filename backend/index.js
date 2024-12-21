12aba8d16bdd3ae5f6a5ecf60c9d5d61f3e89a09
Shivam Jha <shivam09.sj@gmail.com>
2024-12-21 22:57:48 +0530

    Update

--- backend/index.js
+++ backend/index.js
@@ -13,11 +13,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-const corsOptions = {
-  origin: ["https://jobportalfrontend-six.vercel.app/", "http://localhost:5173"],
-  credentials: true,
-};
-app.use(cors(corsOptions));
+// const corsOptions = {
+//   origin: ["https://jobportalfrontend-six.vercel.app/", "http://localhost:5173"],
+//   credentials: true,
+// };
+app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 
